6ac87066311a3714633fadc877cf106367d7de5c
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-09-05 15:13:51 -0400

    Update __init__.py to include RefsX and additional unit error classes in __all__

--- pycuc/docs/__init__.py
+++ pycuc/docs/__init__.py
@@ -2,10 +2,21 @@ from .cuc import CustomUnitConverter
 from .cucx import CustomUnitConverterX
 from .utils import Utils
 from .refs import Refs
+from .refsx import RefsX
+from .unit_errors import (
+    AffineUnitError,
+    CyclicUnitDefinitionError,
+    DimensionMismatchError,
+    UnitConversionError,
+    UnitSyntaxError,
+    UnknownUnitError,
+)
 
 __all__ = [
     'CustomUnitConverter',
     'Utils',
     'CustomUnitConverterX',
-    'Refs'
+    'Refs', 'RefsX',
+    'UnitConversionError', 'UnitSyntaxError', 'UnknownUnitError',
+    'DimensionMismatchError', 'AffineUnitError', 'CyclicUnitDefinitionError'
 ]
